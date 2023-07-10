9cfcc469320fad1e334cf5b2d73a9a9d9ec756b6
Beyram <beyram.taglietti@neosperience.com>
2023-07-10 15:37:38 +0200

    responsive sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <h1 className="text-7xl mb-4">Beyram Taglietti</h1>
-      <h3 className="text-3xl mb-2">Frontend Developer</h3>
-      <h3 className="text-md">(Currently based in 🇮🇹)</h3>
+      <h1 className="text-7xl mb-4 text-center">Beyram Taglietti</h1>
+      <h3 className="text-3xl mb-2 text-center">Frontend Developer</h3>
+      <h3 className="text-md text-center">(Currently based in 🇮🇹)</h3>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,61 +8,39 @@ import Link from 'next/link';
 
 import { usePathname } from 'next/navigation';
 
+const routes = [
+  { label: 'Home', link: '/', icon: <AiFillHome /> },
+  { label: 'About Me', link: '/aboutme', icon: <FaUser /> },
+  { label: 'Projects', link: '/projects', icon: <MdDesktopMac /> },
+  { label: 'Posts', link: '/posts', icon: <IoIosText /> },
+];
+
 const Sidebar = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
   return (
-    <div className="flex flex-row h-[100dvh] w-full">
-      <div className="h-full w-[250px] bg-[#171717] fixed">
-        <ul className="p-3 flex flex-col gap-3">
-          <li>
-            <Link
-              href={'/'}
-              className={`flex flex-row gap-5 items-center cursor-pointer ${
-                pathname === '/' && 'bg-[#404040]'
-              } hover:bg-[#515151] rounded-lg p-2 pl-4`}
-            >
-              <AiFillHome />
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={'/aboutme'}
-              className={`flex flex-row gap-5 items-center cursor-pointer ${
-                pathname === '/aboutme' && 'bg-[#404040]'
-              } hover:bg-[#515151] rounded-lg p-2 pl-4`}
-            >
-              <FaUser />
-              About Me
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={'/projects'}
-              className={`flex flex-row gap-5 items-center cursor-pointer ${
-                pathname === '/projects' && 'bg-[#404040]'
-              } hover:bg-[#515151] rounded-lg p-2 pl-4`}
-            >
-              <MdDesktopMac />
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={'/posts'}
-              className={`flex flex-row gap-5 items-center cursor-pointer ${
-                pathname === '/posts' && 'bg-[#404040]'
-              } hover:bg-[#515151] rounded-lg p-2 pl-4`}
-            >
-              <IoIosText />
-              Posts
-            </Link>
-          </li>
+    <div className="flex h-[100dvh] w-full">
+      <div className="h-[80px] w-full md:h-full md:w-[250px] bg-[#171717] fixed bottom-0 items-center">
+        <ul className="p-3 flex md:flex-col gap-3 justify-evenly md:justify-start h-full items-center md:items-start">
+          {routes.map((route, index) => (
+            <li key={index} className="w-full">
+              <Link
+                href={route.link}
+                className={`flex flex-row gap-5 items-center cursor-pointer ${
+                  pathname === route.link && 'bg-[#404040]'
+                } hover:bg-[#515151] rounded-lg p-2 md:pl-4 text-2xl md:text-base justify-center md:justify-start`}
+              >
+                {route.icon}
+                <span className="hidden md:block">{route.label}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
 
-      <div className="h-full w-full ml-[250px] p-5">{children}</div>
+      <div className="h-full w-full mb-[80px] md:mb-0 md:ml-[250px] p-5">
+        {children}
+      </div>
     </div>
   );
 };
