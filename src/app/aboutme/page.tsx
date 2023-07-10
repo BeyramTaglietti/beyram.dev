d24675009c3f0073edf49fa13e0f298d4a8ca202
Beyram <beyram.taglietti@neosperience.com>
2023-07-10 16:07:35 +0200

    projects page mockup

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About Me',
+};
+
 const AboutMe = () => {
   return (
     <div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,46 @@
+import Image from 'next/image';
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'My Projects',
+};
+
+const projects = [
+  {
+    name: 'Øpia',
+    imageUrl: 'https://texts.com/ogcover.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    name: 'Norse Venture',
+    imageUrl: 'https://texts.com/ogcover.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+];
+
 const Projects = () => {
-  return <h1>Projects page</h1>;
+  return (
+    <>
+      {projects.map((project, index) => (
+        <div className="flex flex-col gap-4" key={index}>
+          <h1 className="text-4xl">{project.name}</h1>
+          <Image
+            src={project.imageUrl}
+            alt={project.name}
+            height={250}
+            width={800}
+          />
+          <p>{project.description}</p>
+          {index % 2 === 0 && (
+            <hr className="h-px border-0 bg-gray-700 my-4"></hr>
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Projects;
