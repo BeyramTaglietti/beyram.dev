3d0fcd73f36cf0d19d8c76b35c9166e9a8b876b2
Beyram <beyram.taglietti@gmail.com>
2024-01-28 15:19:04 +0100

    fix: scroll restoration

--- app/root.tsx
+++ app/root.tsx
@@ -37,13 +37,17 @@ export default function App() {
       </head>
       <body>
         <div className="flex h-[100dvh] w-full flex-col-reverse md:flex-row">
-          <Sidebar />
+          <div className="h-[80px] md:h-full w-full md:w-[250px] 2xl:w-[350px] fixed bottom-0 left-0 md:top-0">
+            <Sidebar />
+          </div>
 
-          <main className="h-full py-5 px-2 overflow-auto lg:px-14 flex-1 flex justify-center">
-            <div className="max-w-[1000px]">
-              <Outlet />
-            </div>
-          </main>
+          <div className="pb-[80px] md:pb-0 pl-0 md:pl-[250px] 2xl:pl-[350px] w-full h-full">
+            <main className="h-full py-5 px-2 lg:px-14 flex-1 flex justify-center">
+              <div className="max-w-[1000px]">
+                <Outlet />
+              </div>
+            </main>
+          </div>
         </div>
         <ScrollRestoration />
         <Scripts />
